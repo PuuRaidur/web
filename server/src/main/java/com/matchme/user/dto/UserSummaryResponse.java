fe85d092dce9726adb4230d3b206b59550f19220
georg <PuuRaidur@users.noreply.github.com>
2026-03-26 01:37:10 +0200

    dto updated

--- server/src/main/java/com/matchme/user/dto/UserSummaryResponse.java
+++ server/src/main/java/com/matchme/user/dto/UserSummaryResponse.java
@@ -3,11 +3,11 @@ package com.matchme.user.dto;
 public class UserSummaryResponse {
     public Long id;
     public String name;
-    public String proofilePictureUrl;
+    public String profilePictureUrl;
 
-    public UserSummaryResponse(Long id, String name, String proofilePictureUrl) {
+    public UserSummaryResponse(Long id, String name, String profilePictureUrl) {
         this.id = id;
         this.name = name;
-        this.proofilePictureUrl = proofilePictureUrl;
+        this.profilePictureUrl = profilePictureUrl;
     }
 }
