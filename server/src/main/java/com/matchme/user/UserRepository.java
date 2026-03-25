32da9eaa41d4491c50a895338ef16abdb4d21fe9
georg <PuuRaidur@users.noreply.github.com>
2026-03-26 01:39:08 +0200

    filter out current user

--- server/src/main/java/com/matchme/user/UserRepository.java
+++ server/src/main/java/com/matchme/user/UserRepository.java
@@ -2,8 +2,12 @@ package com.matchme.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    // Find all users except the one with the given id (for recommendations)
+    List<User> findAllByIdNot(Long excludeId);
 }
