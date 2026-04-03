525be0e18c5630e1ffa2e9e8668ce39911b63289
georg <PuuRaidur@users.noreply.github.com>
2026-04-04 02:00:22 +0300

    connection request response

---
+++ server/src/main/java/com/matchme/connection/dto/ConnectionRequestResponse.java
@@ -0,0 +1,9 @@
+package com.matchme.connection.dto;
+
+public class ConnectionRequestResponse {
+    public Long id;
+
+    public ConnectionRequestResponse(Long id) {
+        this.id = id;
+    }
+}
